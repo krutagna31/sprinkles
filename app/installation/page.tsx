c910257cf40255587b491fd6b9d5952ed7716fb1
Krutagna Patel <krutagnapatel31@gmail.com>
2025-08-31 18:24:13 +0530

    feat: add installation page component ✨

---
+++ app/installation/page.tsx
@@ -0,0 +1,13 @@
+import { SectionContainer, ViewContainer } from "@/components/layouts";
+
+export default function InstallationPage() {
+  return (
+    <>
+      <SectionContainer>
+        <ViewContainer className="space-y-2">
+          <h1>Installation Page</h1>
+        </ViewContainer>
+      </SectionContainer>
+    </>
+  );
+}
