e0ce6eba7dc9c15c598229cdff9c39ff10fabf1d
Krutagna Patel <krutagnapatel31@gmail.com>
2025-08-31 19:20:32 +0530

    feat: add variants to view-container ui component ✨

--- components/layouts/view-container.tsx
+++ components/layouts/view-container.tsx
@@ -1,12 +1,27 @@
-import clsx from "clsx";
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+
+const containerVariants = cva("mx-auto px-4", {
+  variants: {
+    size: {
+      default: "max-w-5xl",
+      sm: "max-w-3xl",
+      lg: "max-w-7xl",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
 
 function ViewContainer({
   children,
   className,
+  size,
   ...props
-}: React.ComponentProps<"div">) {
+}: React.ComponentProps<"div"> & VariantProps<typeof containerVariants>) {
   return (
-    <div className={clsx(className, "max-w-7xl mx-auto px-4")} {...props}>
+    <div className={cn(containerVariants({ className, size }))} {...props}>
       {children}
     </div>
   );
